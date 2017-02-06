5f9435a99455a92e399e719975f8afd4bf3256b4
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-02-06 01:15:56 -0500

    time r001 45 in-progress

--- ruby/r001.rb
+++ ruby/r001.rb
@@ -1,5 +1,18 @@
+#gives us array for commit data
 log = ARGF.readlines
+timeCommits = []
 
-log.gsub! 'commit', '\ncommit'
+log.each_with_index do |thing,index|
+    if(log[index]start_with?('Author: Hussein Fahmy'))
+    	timeCommits.append(log[index-1])
+    	timeCommits.append(log[index])
+    	timeCommits.append(log[index+1])
+    	timeCommits.append(log[index+2])
+    end
+end
+
+print timeCommits
+
+#log.gsub! 'commit', '\ncommit'
 #print the git log to make sure its right
-print inputArray
+#print log
