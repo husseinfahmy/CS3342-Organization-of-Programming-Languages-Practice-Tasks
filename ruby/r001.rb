ffa1cff30ad3a4c72fe00fce72993987c239bbb5
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-02-09 11:20:10 -0500

    time r001 60 in-progress

--- ruby/r001.rb
+++ ruby/r001.rb
@@ -1,18 +1,38 @@
 #gives us array for commit data
 log = ARGF.readlines
 timeCommits = []
+i = 0
 
 log.each_with_index do |thing,index|
-    if(log[index]start_with?('Author: Hussein Fahmy'))
-    	timeCommits.append(log[index-1])
-    	timeCommits.append(log[index])
-    	timeCommits.append(log[index+1])
-    	timeCommits.append(log[index+2])
-    end
+    next unless (log[index].start_with? 'Author: Hussein Fahmy')&&(log[index-1].start_with? 'Commit')
+    	timeCommits[i] = log[index-1]
+    	i += 1
+    	timeCommits[i] = log[index]
+    	i += 1
+    	timeCommits[i] = log[index+1]
+    	i += 1
+    	timeCommits[i] = log[index+3]
+    	index += 5
+    	i += 1
 end
 
-print timeCommits
+#print log
 
+log.each_with_index do |thing,index|
+	if thing != nil
+		print "Element: ", index
+		print timeCommits[index]
+		puts "\n"
+
+	end
+	
+
+
+	#if(index%4 == 0)
+	#	puts "\n"
+	#end
+end
 #log.gsub! 'commit', '\ncommit'
 #print the git log to make sure its right
 #print log
+
