67b9e1352a3bcfa32f73b80ff61039e09c244169
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-02-12 20:49:07 -0500

    time r001 60 in-progress

--- ruby/r001.rb
+++ ruby/r001.rb
@@ -24,24 +24,37 @@ log.each_with_index do |thing,index|
     	timeCommits[i] = log[index+3]
     	index += 5
     	i += 1
-end
+    end
 
 #print log
 
 timeCommits = timeCommits.reverse
 
 timeCommits.each_with_index do |thing,index|
-	puts timeCommits[index]
+	#puts timeCommits[index]
 	
 
-    if((index)%4 == 0)
-        puts timeCommits[index]
-        puts "\n\n\n\n"
+    if((index)%4 == 0)#fourth element is the commit message
+        #prints the commit messages
+        #puts timeCommits[index]
+        #puts "\n\n\n\n"
 
-        #words = timeCommits[index].split
-        #words.each_with_index do |word, index|
-        #    print word
-        #end
+        #tokenize the commit message
+        words = timeCommits[index].split
+        words.each_with_index do |word, sentenceIndex|
+            if words[1].start_with? 'j'#if javascript task
+                #code
+                print timeCommits[index], "\n"
+            end
+            if words[1].start_with? 'r'#if ruby task
+                #code
+            end
+            if words[1].start_with? 'h'#if haskell task
+                #code
+            end
+
+
+        end
     end
 
 
