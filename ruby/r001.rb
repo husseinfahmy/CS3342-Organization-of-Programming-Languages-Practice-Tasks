ae07f6f0cf16d44315a1cc1bf04b8324bad2c2a5
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-02-19 17:19:51 -0500

    time r001 60 in-progress

--- ruby/r001.rb
+++ ruby/r001.rb
@@ -5,7 +5,6 @@ i = 0
 
 
 
-puts "-------------------------------------------------------------------------------------"
 
 
 log.each_with_index do |thing,index|
@@ -15,54 +14,86 @@ log.each_with_index do |thing,index|
 
 
     next unless (log[index].start_with? 'Author: Hussein Fahmy')&&(log[index+3].start_with? '    time')
-    	timeCommits[i] = log[index-1]
-    	i += 1
-    	timeCommits[i] = log[index]
-    	i += 1
-    	timeCommits[i] = log[index+1]
-    	i += 1
-    	timeCommits[i] = log[index+3]
-    	index += 5
-    	i += 1
+        timeCommits[i] = log[index-1]
+        i += 1
+        timeCommits[i] = log[index]
+        i += 1
+        timeCommits[i] = log[index+1]
+        i += 1
+        timeCommits[i] = log[index+3]
+        index += 5
+        i += 1
     end
 
 #print log
 
 timeCommits = timeCommits.reverse
 
+
+
+######################################
+############ ALL JS TASKS ############
+######################################
+
+puts "------------------------------------------"
+puts "\t\tJAVASCRIPT"
+puts "------------------------------------------"
+
 timeCommits.each_with_index do |thing,index|
-	#puts timeCommits[index]
-	
-
+   
     if((index)%4 == 0)#fourth element is the commit message
-        #prints the commit messages
-        #puts timeCommits[index]
-        #puts "\n\n\n\n"
-
+        
         #tokenize the commit message
         words = timeCommits[index].split
-        words.each_with_index do |word, sentenceIndex|
-            if words[1].start_with? 'j'#if javascript task
-                #code
-                print timeCommits[index], "\n"
-            end
-            if words[1].start_with? 'r'#if ruby task
-                #code
-            end
-            if words[1].start_with? 'h'#if haskell task
-                #code
-            end
-
-
+        
+        if words[1].start_with? 'j'#if javascript task
+            #code
+            print timeCommits[index], "\n"
         end
     end
-
-
-	#if((index+1)%4 == 0)
-	#	puts "\n\n\n\n\n"
-	#end
 end
-#log.gsub! 'commit', '\ncommit'
-#print the git log to make sure its right
-#print log
 
+
+######################################
+########### ALL RUBY TASKS ###########
+######################################
+
+puts "------------------------------------------"
+puts "\t\tRUBY"
+puts "------------------------------------------"
+
+timeCommits.each_with_index do |thing,index|
+   
+    if((index)%4 == 0)#fourth element is the commit message
+        
+        #tokenize the commit message
+        words = timeCommits[index].split
+        
+        if words[1].start_with? 'r'#if Ruby task
+            #code
+            print timeCommits[index], "\n"
+        end
+    end
+end
+
+######################################
+########## ALL HASKELL TASKS #########
+######################################
+
+puts "------------------------------------------"
+puts "\t\tHASKELL"
+puts "------------------------------------------"
+
+timeCommits.each_with_index do |thing,index|
+   
+    if((index)%4 == 0)#fourth element is the commit message
+        
+        #tokenize the commit message
+        words = timeCommits[index].split
+        
+        if words[1].start_with? 'h'#if haskell task
+            #code
+            print timeCommits[index], "\n"
+        end
+    end
+end
