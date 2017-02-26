e947c64147a697e16c35fd3e8c516691538bbd00
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-02-26 15:58:20 -0500

    time 60 r001 in-progress

--- ruby/r001.rb
+++ ruby/r001.rb
@@ -3,8 +3,10 @@
 timeCommits = []
 i = 0
 
-value = %x(git log)
-puts value.kind_of?(Array)
+puts 
+logString = %x(git log)
+log = logString.split("\n")
+
 
 log.each_with_index do |thing,index|
 
@@ -20,75 +22,84 @@ log.each_with_index do |thing,index|
         i += 1
     end
 
-#print log
 
 timeCommits = timeCommits.reverse
 
 
+if ARGV.size == 1
+    puts "1 ARGUMENT"
+elsif ARGV.size ==0 
+    ######################################
+    ############ ALL JS TASKS ############
+    ######################################
 
-######################################
-############ ALL JS TASKS ############
-######################################
+    puts "------------------------------------------"
+    puts "\t\tJAVASCRIPT"
+    puts "------------------------------------------"
 
-puts "------------------------------------------"
-puts "\t\tJAVASCRIPT"
-puts "------------------------------------------"
-
-timeCommits.each_with_index do |thing,index|
-   
-    if((index)%4 == 0)#fourth element is the commit message
-        
-        #tokenize the commit message
-        words = timeCommits[index].split
-        
-        if words[1].start_with? 'j'#if javascript task
-            #code
-            print timeCommits[index], "\n"
+    timeCommits.each_with_index do |thing,index|
+       
+        if((index)%4 == 0)#fourth element is the commit message
+            
+            #tokenize the commit message
+            words = timeCommits[index].split
+            
+            if words[1].start_with? 'j'#if javascript task
+                #code
+                print timeCommits[index], "\n"
+            end
         end
     end
-end
 
 
-######################################
-########### ALL RUBY TASKS ###########
-######################################
+    ######################################
+    ########### ALL RUBY TASKS ###########
+    ######################################
 
-puts "------------------------------------------"
-puts "\t\tRUBY"
-puts "------------------------------------------"
+    puts "------------------------------------------"
+    puts "\t\tRUBY"
+    puts "------------------------------------------"
 
-timeCommits.each_with_index do |thing,index|
-   
-    if((index)%4 == 0)#fourth element is the commit message
-        
-        #tokenize the commit message
-        words = timeCommits[index].split
-        
-        if words[1].start_with? 'r'#if Ruby task
-            #code
-            print timeCommits[index], "\n"
+    timeCommits.each_with_index do |thing,index|
+       
+        if((index)%4 == 0)#fourth element is the commit message
+            
+            #tokenize the commit message
+            words = timeCommits[index].split
+            
+            if words[1].start_with? 'r'#if Ruby task
+                #code
+                print timeCommits[index], "\n"
+            end
         end
     end
-end
 
-######################################
-########## ALL HASKELL TASKS #########
-######################################
+    ######################################
+    ########## ALL HASKELL TASKS #########
+    ######################################
 
-puts "------------------------------------------"
-puts "\t\tHASKELL"
-puts "------------------------------------------"
+    puts "------------------------------------------"
+    puts "\t\tHASKELL"
+    puts "------------------------------------------"
 
-timeCommits.each_with_index do |thing,index|
-   
-    if((index)%4 == 0)#fourth element is the commit message
-        
-        #tokenize the commit message
-        words = timeCommits[index].split
-        
-        if words[1].start_with? 'h'#if haskell task
-            #code
-            print timeCommits[index], "\n"
+    timeCommits.each_with_index do |thing,index|
+       
+        if((index)%4 == 0)#fourth element is the commit message
+            
+            #tokenize the commit message
+            words = timeCommits[index].split
+            
+            if words[1].start_with? 'h'#if haskell task
+                #code
+                print timeCommits[index], "\n"
+            end
         end
     end
+else
+    puts "Only 1 argument is allowed!"
 end
+    
+
+
+
+
