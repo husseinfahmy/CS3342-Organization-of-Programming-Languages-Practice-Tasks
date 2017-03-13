345919c9023874e615b8a6ddda1ebe71d4874d6b
Hussein Fahmy <fahmy.hussein@hotmail.com>
2017-03-12 20:41:40 -0400

    time h001 90 in-progres

--- haskell/h001/h001.hs
+++ haskell/h001/h001.hs
@@ -1,4 +1,14 @@
 import System.Process
+--import Data.List.Split
+
+
+split :: String -> Char -> [String]
+split "" _ = []
+split xs c = let (ys, zs) = break (== c) xs
+             in  if null zs then [ys] else ys : split (tail zs) c
+
 main = do
-	let log = readProcess "git" ["log"] []
-	print log
+	log <- readProcess "git" ["log"] []
+--	splitOn:: log '\n' log
+	logList <- show (split "HI \n hey" '\n')
+	print logList
